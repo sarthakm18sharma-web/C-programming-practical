f3a4580d3cf74eff0564901aee35f7eaaf3d0f14
Sarthak Sharma <sarthakm18sharma@gmail.com>
2026-08-07 11:01:34 +0530

    Added even or odd program

---
+++ conditions/even_or_odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+
+{ int a;
+    printf("enter a number");
+ scanf("%d",&a);
+ if (a%2==0) {
+      printf("even number");
+      }
+ else{printf("odd number");
+       }
+}
